44889dbe4403facb85eac9eaabf2d55d989152e4
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-09-13 19:47:01 +1000

    feat: Move the HomepageHeader component to component folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,13 @@
 import type { ReactNode } from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
-
-import styles from "./index.module.css";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Read the notes 📝
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import HomepageHeader from "@site/src/components/HomepageHeader";
 
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-    >
+    <Layout title={`${siteConfig.title}`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
