0b3d09d3d20b4e5f4958ec998c936a55fb0d1c1d
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-09-14 14:08:04 +1000

    feat: Add Hero component

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,15 +18,33 @@ const config: Config = {
       },
       innerHTML: JSON.stringify({
         "@context": "https://schema.org/",
-        "@type": "WebSite",
-        name: "Rendy E. Saputra's Site",
-        url: "https://res.id.au/",
-        alternateName: ["Rendy E. Saputra", "Rendy E. Saputra's Portfolio"],
-        potentialAction: {
-          "@type": "SearchAction",
-          target: "https://res.id.au/search?q={search_term_string}",
-          "query-input": "required name=search_term_string",
-        },
+        "@graph": [
+          {
+            "@type": "WebSite",
+            "@id": "https://res.id.au/#website",
+            name: "Rendy E. Saputra's Site",
+            url: "https://res.id.au/",
+            alternateName: ["Rendy E. Saputra", "Rendy E. Saputra's Portfolio"],
+            publisher: { "@id": "https://res.id.au/#person" },
+            potentialAction: {
+              "@type": "SearchAction",
+              target: "https://res.id.au/search?q={search_term_string}",
+              "query-input": "required name=search_term_string",
+            },
+          },
+          {
+            "@type": "Person",
+            "@id": "https://res.id.au/#person",
+            name: "Rendy E. Saputra",
+            url: "https://res.id.au/",
+            image: "https://res.id.au/images/ava.jpg",
+            jobTitle: "Software Engineer",
+            sameAs: [
+              "https://github.com/rensaputra",
+              "https://www.linkedin.com/in/rendyekasaputra/",
+            ],
+          },
+        ],
       }),
     },
     {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,13 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import HomepageHeader from "@site/src/components/HomepageHeader";
+import Hero from "@site/src/components/Hero";
 
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title={`${siteConfig.title}`}>
-      <HomepageHeader />
+      <Hero />
       <main>
         <HomepageFeatures />
       </main>
